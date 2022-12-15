51db368933d8c73d0bf4b947c067a801f82aa8a4
GonzaloFdezMoreno <gonzaf05@ucm.es>
2022-12-15 18:23:35 +0100

    arreglado los limitas del laser y el dash con danyo

--- src/dashield.js
+++ src/dashield.js
@@ -1,4 +1,4 @@
-export default class Laser extends Phaser.GameObjects.Sprite {
+export default class Dashield extends Phaser.GameObjects.Sprite {
     /**
      * Constructor de Hook
      * @param {Sceme} scene Escena en la que aparece el gancho
@@ -7,7 +7,7 @@ export default class Laser extends Phaser.GameObjects.Sprite {
      * @param {Player} player el jugador
      */
     constructor(scene, x, y,scal){
-        super(scene, x, y,scal,'laserp');
+        super(scene, x, y);
        
         this.setOrigin(0,0.5)
         this.scene.add.existing(this);
@@ -15,13 +15,12 @@ export default class Laser extends Phaser.GameObjects.Sprite {
         this.body.setGravity(0,-100);
         //this.body.setCollideWorldBounds(false,false);
         this.scaleX=1;
-        this.dir=scal;
-        this.elong=7;
+       
        
 
         //this.graph = this.scene.add.graphics({ lineStyle: { width: 4, color: 0xaa00aa } });
         //this.line = this.scene.add.existing(new Phaser.Geom.Line(this.x, 500, this.x, this.y));
-        this.body.setVelocity(0, 0);
+        
 
         //esto es para mostrar una linea de disparo, no tiene colisiones
         /*this.line = new Phaser.Geom.Line(400, this.y, this.x, this.y);
@@ -31,27 +30,6 @@ export default class Laser extends Phaser.GameObjects.Sprite {
         //this.scene.physics.add.existing(this.line);
         //this.scene.physics.add.collider(this.graphics, this.scene.balls);
         
-
-
-        this.charge=this.scene.tweens.add({
-            targets:[]
-        })
-
-        this.scene.anims.create({
-            key: 'attack',
-            frames: scene.anims.generateFrameNumbers('laserp', { start: 0, end: 1 }),
-            frameRate: 20,
-            repeat: -1
-          
-            })
-        
-              this.play('attack');
-
-        //for(let i=0;i<10;i++){
-        this.charging();
-       // }
-
-    
     }
     
     ent(){
@@ -107,19 +85,20 @@ export default class Laser extends Phaser.GameObjects.Sprite {
        
 
             let timer=this.scene.time.addEvent({
-                delay: 1000, 
+                delay: 200, 
                 callback: this.onDestroy,
                 callbackScope: this,
                 
             });
             
-        
+        this.x=this.scene.player.x;
+        this.y=this.scene.player.y;
 
         this.scene.physics.add.collider(this.scene.allEnemies, this, (o1, o2) => {
             // hacer algo
             
-            o1.minusHP(5); 
-            this.body.setVelocity(0, 0);
+            o1.minusHP(2); 
+            
             //this.onDestroy();
         });
 
